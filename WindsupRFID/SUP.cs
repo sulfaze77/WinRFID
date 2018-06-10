f022cbc58d55e97fee9dd17387d425f305ca205f
yorick <yorick.droz@windsup.ch>
2018-06-10 21:20:05 +0200

    created classDiagram bases, started work on UI

---
+++ WindsupRFID/SUP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindsupRFID
+{
+    public class SUP
+    {
+        private int ID;
+        private SupTypes Type;
+
+        public SUP(int _ID, SupTypes _Type)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WindsupRFID/SUPTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindsupRFID
+{
+
+    public class SupTypes
+    {
+        private int Demi;
+        private int Hour;
+        private int Supp;
+        private string Name;
+
+        public SupTypes(string N, int D, int H, int S)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
